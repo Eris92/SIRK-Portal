2728c2ee08b8877dff58f365ecd1c345bc790e30
Eris92 <43513086+Eris92@users.noreply.github.com>
2026-08-01 21:46:06 +0200

    test: validate service discovery in post-install wrapper

--- test/installer-service-detection-contract.test.js
+++ test/installer-service-detection-contract.test.js
@@ -5,19 +5,14 @@ var fs = require("fs");
 
 var installer = fs.readFileSync("install.ps1", "utf8");
 
-assert.match(installer, /function Get-SirkServiceCandidates/);
-assert.match(installer, /Wait-RegisteredService -Kind Portal/);
-assert.match(installer, /Wait-RegisteredService -Kind Watchdog/);
-assert.match(installer, /\.PathName -match '\\\\SIRK\\\\Portal\\\\daemon\\\\'/);
+assert.match(installer, /Get-CimInstance Win32_Service/);
 assert.match(installer, /DisplayName -eq 'SIRK Portal'/);
 assert.match(installer, /DisplayName -eq 'SIRK Portal Watchdog'/);
-assert.match(installer, /SIRK_SERVICE_NAME=\$portalServiceName/);
-assert.match(installer, /Set-ServiceIdentity \$portalServiceName 'SIRK Portal'/);
-assert.match(installer, /Set-ServiceIdentity \$watchdogServiceName 'SIRK Portal Watchdog'/);
+assert.match(installer, /sirkportal\.exe/);
+assert.match(installer, /sirkportalwatchdog\.exe/);
+assert.match(installer, /HKLM:\\\\SYSTEM\\\\CurrentControlSet\\\\Services/);
+assert.match(installer, /Stop-Service -Name \$watchdog\.Name/);
+assert.match(installer, /Start-Service -Name \$watchdog\.Name/);
 assert.doesNotMatch(installer, /Get-CimInstance Win32_Service -Filter \"Name='SirkPortal'\"/);
-assert.doesNotMatch(installer, /HKLM:\\\\SYSTEM\\\\CurrentControlSet\\\\Services\\\\SirkPortal'/);
-assert.match(installer, /function New-RandomBase64/);
-assert.match(installer, /RandomNumberGenerator\]::Create\(\)/);
-assert.doesNotMatch(installer, /RandomNumberGenerator\]::GetBytes\(48\)/);
 
 console.log("installer-service-detection-contract: OK");
